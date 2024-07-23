8195d72b71e4228450dba2742165dc7d54212599
cr7st74n <34308684+cr7st74n@users.noreply.github.com>
2024-07-23 10:49:26 -0400

    setting up the code to solve the problem

---
+++ src/LinkedList.java
@@ -0,0 +1,64 @@
+package ds;
+
+public class LinkedList {
+	
+	public ListNode head;
+	
+	public LinkedList () {
+		head = null;
+	}
+	
+	/*
+	 * Implement the LIST-SEARCH(L, k) function
+	 */
+	public ListNode search (int k) {
+		
+	}
+	
+	/*
+	 * Implement the LIST-INSERT(L, x) function
+	 * Note that x is a integer value, not a ListNode
+	 */
+	public void insert (int x) {
+		
+	}
+	
+	/*
+	 * Implement the LIST-DELETE(L, x) function
+	 */
+	public void delete (ListNode x) {
+		
+	}
+	
+	
+	public String toString () {
+		String str;
+		ListNode x;
+		
+		str = "[";
+		x = this.head;
+		while (x != null) {
+			str += x.key + ",";
+			x = x.next;
+		}
+		
+		str += "]";
+		return str;
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LinkedList l;
+		
+		l = new LinkedList();
+		for (int i = 0; i < 5; i++)
+			l.insert(i);
+		System.out.println(l.toString());
+		for (int i = 0; i < 2; i++) 
+			l.delete(l.head.next);
+		System.out.println(l.toString());
+	}
+
+}
